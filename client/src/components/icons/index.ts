b88779cffa3d2e2945877968fe1a2deced7dd7a3
Akshatha Dungi <dungiakshatha@gmail.com>
2026-05-31 16:24:08 +0530

    Remove inconsistencies

--- client/src/components/icons/index.ts
+++
@@ -1,6 +0,0 @@
-// Icon Components
-export { SendIcon } from './SendIcon'
-export { UploadIcon } from './UploadIcon'
-export { EditIcon } from './EditIcon'
-export { CopyIcon } from './CopyIcon'
-export { CloseIcon } from './CloseIcon'
--- client/src/components/ui/index.ts
+++
@@ -1,5 +0,0 @@
-// UI Components
-export { Button } from './Button'
-export { Input } from './Input'
-export { MessageBubble } from './MessageBubble'
-export { FileUpload } from './FileUpload'
